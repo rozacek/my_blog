464f198a8e4f7b4c883cd1c39b665c3a400baa92
epawroz <rozacek@gmail.com>
2017-11-08 11:13:47 +0100

    Config for mysql database.

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -71,12 +72,12 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/rozacek/apps/conf/mysql.cnf',
+        },
     }
 }
 
